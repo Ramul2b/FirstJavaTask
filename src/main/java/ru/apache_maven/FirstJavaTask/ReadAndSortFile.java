7135bf3e8a8531215e8920ceb0d402453efc17c3
Ramul2B <ramul2b@mail.ru>
2015-08-29 14:11:38 +0300

    First commit.

---
+++ src/main/java/ru/apache_maven/FirstJavaTask/ReadAndSortFile.java
@@ -0,0 +1,13 @@
+package ru.apache_maven.FirstJavaTask;
+
+/**
+ * Hello world!
+ *
+ */
+public class ReadAndSortFile 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
