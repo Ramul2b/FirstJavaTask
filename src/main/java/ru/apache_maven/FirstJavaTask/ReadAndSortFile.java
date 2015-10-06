688387df0f69f98dae6ca833d907ff4c73db1491
Ramul2B <ramul2b@mail.ru>
2015-10-06 12:54:29 +0300

    Fixed formatting errors.

--- src/main/java/ru/apache_maven/FirstJavaTask/ReadAndSortFile.java
+++ src/main/java/ru/apache_maven/FirstJavaTask/ReadAndSortFile.java
@@ -1,3 +1,4 @@
+
 package ru.apache_maven.FirstJavaTask;
 
 import java.io.BufferedReader;
@@ -8,102 +9,119 @@ import java.util.Collections;
 import java.util.ArrayList;
 import java.io.IOException;
 
-/**Reads a strings from a text file input.txt, sort and write them into the file output.txt.
-
-*@version 1.8.
-*@author Vdovichenko A.A.
-*/
-public class ReadAndSortFile{
-	
 /**
- * Reads a string of characters in the text file input.txt and writes them into an array. 
- * The file specified in the run configuration.
- * @param inputTextFile - text file input.txt from which the symbols are read. 
- * @return textArray - array of sorted rows. For sorting used static method {@link sort}.
- * @throws IOException  when an IO error occurs. Triggered when not right given the way of the files.
+ * Reads a strings from a text file input.txt, sort and write them into the file
+ * output.txt.
+ * 
+ * @version 1.8.
+ * @author Vdovichenko A.A.
  */
-  public static ArrayList<String> writeInArrayAndSort (String inputTextFile){
+public class ReadAndSortFile {
 
-/**
- * ArrayList - no fixed-size array.
- * An array in which to store the character string taken from a file.
- */
-     ArrayList<String> textArray = new ArrayList<String>();
+	/**
+	 * Reads a string of characters in the text file input.txt and writes them into
+	 * an array.
+	 * The file specified in the run configuration.
+	 * 
+	 * @param inputTextFile - text file input.txt from which the symbols are read.
+	 * @return textArray - array of sorted rows. For sorting used static method
+	 *         {@link sort}.
+	 * @throws IOException when an IO error occurs. Triggered when not right given
+	 *         the way of the files.
+	 */
+	public static ArrayList<String> writeInArrayAndSort(String inputTextFile) {
 
-/**Started handling of exceptions.*/
-     try{
-		 
-/**	   
- * Class BufferedReader reads characters from the input character stream and buffering to effectively read them.
- * Class FileReader used to read the character (text) files.
- */
-       BufferedReader readTextFile = new BufferedReader(new FileReader(inputTextFile));
-				
-/** Strings of characters taken from the file. */
-       String stringOfSymbols;
-				
-/** 
- * As long as a character string not null, they are added in the array textArray.
- * This means that an array of lines from input.txt.do recorded as long as have something recording.
- */
-       while((stringOfSymbols = readTextFile.readLine())!=null)
-         textArray.add(stringOfSymbols);
-				
-/** Closes the input stream of symbols.*/
-       readTextFile.close();
+		/**
+		 * ArrayList - no fixed-size array.
+		 * An array in which to store the character string taken from a file.
+		 */
+		ArrayList<String> textArray = new ArrayList<String>();
 
-/** Sorting an array.*/
-       Collections.sort(textArray);}   
+		/** Started handling of exceptions. */
+		try {
 
-/** Handling of exceptions*/
-       catch (IOException input){
-         System.out.println("File is not founded.");
-       }
+			/**
+			 * Class BufferedReader reads characters from the input character stream
+			 * and buffering to effectively read them.
+			 * Class FileReader used to read the character (text) files.
+			 */
+			BufferedReader readTextFile = new BufferedReader(new FileReader(
+					inputTextFile));
 
-/**Returns a sorted array.*/
-       return textArray;
-	  }
+			/** Strings of characters taken from the file. */
+			String stringOfSymbols;
 
-/**
- * Records sorted lines to the file output.txt. The file specified in the run configuration.
- * @param textArray - an array that contains a sorted string.
- * @param outputTextFile - a text file in which records sorted lines.
- * @throws IOException if an error occurs IO. Triggered when not right given the way the files.
- */
-  public static void writeInOutputFile (ArrayList<String> textArray, String outputTextFile){
+			/**
+			 * As long as a character string not null, they are added in the array
+			 * textArray.
+			 * This means that an array of lines from input.txt.do recorded as long
+			 * as have something recording.
+			 */
+			while ((stringOfSymbols = readTextFile.readLine()) != null)
+				textArray.add(stringOfSymbols);
 
-/** 
- * Class BufferedWriter writes characters to the output character stream and 
- * their buffering for recording. 
- */
-		  
-     try{
-       BufferedWriter writeTextFile = new BufferedWriter(new FileWriter(outputTextFile)); 
+			/** Closes the input stream of symbols. */
+			readTextFile.close();
 
-/** The size of an array of character strings.*/
-       int arraySize = textArray.size(); 
+			/** Sorting an array. */
+			Collections.sort(textArray);
+		}
 
-/** Recording an array to file output.txt.*/
-       for (int i = 0; i < arraySize; i++)           		
-         writeTextFile.write(textArray.get(i) + "\r\n"); 
+		/** Handling of exceptions */
+		catch (IOException input) {
+			System.out.println("File is not founded.");
+		}
 
-/** Closing the output symbol stream and handling of exceptions.*/
-       writeTextFile.close();
-       } catch (IOException output){
-         System.out.println("File is not founded.");
-	   }   
-	   
-  }	
-			
-  public static void main(String[] args){
-		  
-/** Handling of exceptions.*/
-       try{
-         writeInOutputFile(writeInArrayAndSort(args[0]), args[1]);
-       } catch (ArrayIndexOutOfBoundsException Arr){
-          System.out.println("Index of args[] is wrong. Check path-name of files.");
-        }
-	   
-  }
-	  
+		/** Returns a sorted array. */
+		return textArray;
+	}
+
+	/**
+	 * Records sorted lines to the file output.txt. The file specified in the run
+	 * configuration.
+	 * 
+	 * @param textArray - an array that contains a sorted string.
+	 * @param outputTextFile - a text file in which records sorted lines.
+	 * @throws IOException if an error occurs IO. Triggered when not right given the
+	 *         way the files.
+	 */
+	public static void writeInOutputFile(ArrayList<String> textArray,
+			String outputTextFile) {
+
+		/**
+		 * Class BufferedWriter writes characters to the output character stream and
+		 * their buffering for recording.
+		 */
+
+		try {
+			BufferedWriter writeTextFile = new BufferedWriter(new FileWriter(
+					outputTextFile));
+
+			/** The size of an array of character strings. */
+			int arraySize = textArray.size();
+
+			/** Recording an array to file output.txt. */
+			for (int i = 0; i < arraySize; i++)
+				writeTextFile.write(textArray.get(i) + "\r\n");
+
+			/** Closing the output symbol stream and handling of exceptions. */
+			writeTextFile.close();
+		} catch (IOException output) {
+			System.out.println("File is not founded.");
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		/** Handling of exceptions. */
+		try {
+			writeInOutputFile(writeInArrayAndSort(args[0]), args[1]);
+		} catch (ArrayIndexOutOfBoundsException Arr) {
+			System.out
+					.println("Index of args[] is wrong. Check path-name of files.");
+		}
+
+	}
+
 }
--- src/test/java/ru/apache_maven/FirstJavaTask/ReadAndSortTest.java
+++ src/test/java/ru/apache_maven/FirstJavaTask/ReadAndSortTest.java
@@ -1,3 +1,4 @@
+
 package ru.apache_maven.FirstJavaTask;
 
 import junit.framework.Test;
@@ -12,77 +13,84 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class ReadAndSortTest extends TestCase{
+public class ReadAndSortTest extends TestCase {
 
-/** Setting user directory. */
- String userDirectory = System.getProperty("user.dir");
+	/** Setting user directory. */
+	String	userDirectory	= System.getProperty("user.dir");
 
-/** Created text file. */
- File fileInUserDir = new File(userDirectory + "/testOfClass_In.txt");
+	/** Created text file. */
+	File	fileInUserDir	= new File(userDirectory + "/testOfClass_In.txt");
 
-  public ReadAndSortTest ( String testName ){   
-    super( testName );
-  }
+	public ReadAndSortTest(String testName) {
 
-/** Testing program in the directory on home computer.*/
-  public void testWriteAndSort(){
+		super(testName);
+	}
 
-    ArrayList<String> asd, zxc;
-    	
-    asd = ReadAndSortFile.writeInArrayAndSort( "A:\\test_in.txt" );
-    zxc = ReadAndSortFile.writeInArrayAndSort( "A:\\test_out.txt" );
-    	
-    assertTrue( asd.size() == zxc.size() );
-  }
-    
-/** Testing program in the user directory.*/
-  public void testWriteAndSortAgain(){
-    	
-    Random random = new Random();
-    int numeral;
+	/** Testing program in the directory on home computer. */
+	public void testWriteAndSort() {
 
-      try{
+		ArrayList<String> asd, zxc;
 
-/** Creating a text files in the user directory. In one of the files written random numbers*/
-        BufferedWriter writeTextFile_Out = new BufferedWriter(new FileWriter(userDirectory + "/testOfClass_Out.txt"));
-        BufferedWriter writeTextFile_In = new BufferedWriter(new FileWriter(userDirectory + "/testOfClass_In.txt"));
-    			
-        for(int i=0; i<5; i++){
-          numeral = random.nextInt(25);
-          writeTextFile_In.write(numeral);
-        }
+		asd = ReadAndSortFile.writeInArrayAndSort("A:\\test_in.txt");
+		zxc = ReadAndSortFile.writeInArrayAndSort("A:\\test_out.txt");
 
-        ArrayList<String> outFile, inFile;
-    	
-        outFile = ReadAndSortFile.writeInArrayAndSort( userDirectory + "/testOfClass_Out.txt" );
-        inFile = ReadAndSortFile.writeInArrayAndSort( userDirectory + "/testOfClass_In.txt" );
-    	
-/** Compares the size of the two files. */
-        assertTrue( outFile.size() == inFile.size() );    	
-    	
-        writeTextFile_Out.close();
-        writeTextFile_In.close();
-      } catch (IOException input){
-        System.out.println("Something is wrong");
-       }
-  }
+		assertTrue(asd.size() == zxc.size());
+	}
 
+	/** Testing program in the user directory. */
+	public void testWriteAndSortAgain() {
 
-  public static Test suite(){
-    return new TestSuite( ReadAndSortTest .class );
-  }
+		Random random = new Random();
+		int numeral;
 
-    
-  public void testApp(){
-        
-    TestRunner runner = new TestRunner();
-    TestSuite suite = new TestSuite();
-        
-    suite.addTest(new ReadAndSortTest("testWriteAndSort"));
-        
-    suite.addTest(new ReadAndSortTest("testWriteAndSortAgain"));
- 
-    runner.doRun(suite);
-  }
-  
+		try {
+
+			/**
+			 * Creating a text files in the user directory. In one of the files
+			 * written random numbers
+			 */
+			BufferedWriter writeTextFile_Out = new BufferedWriter(
+					new FileWriter(userDirectory + "/testOfClass_Out.txt"));
+			BufferedWriter writeTextFile_In = new BufferedWriter(
+					new FileWriter(userDirectory + "/testOfClass_In.txt"));
+
+			for (int i = 0; i < 5; i++) {
+				numeral = random.nextInt(25);
+				writeTextFile_In.write(numeral);
+			}
+
+			ArrayList<String> outFile, inFile;
+
+			outFile = ReadAndSortFile.writeInArrayAndSort(userDirectory
+					+ "/testOfClass_Out.txt");
+			inFile = ReadAndSortFile.writeInArrayAndSort(userDirectory
+					+ "/testOfClass_In.txt");
+
+			/** Compares the size of the two files. */
+			assertTrue(outFile.size() == inFile.size());
+
+			writeTextFile_Out.close();
+			writeTextFile_In.close();
+		} catch (IOException input) {
+			System.out.println("Something is wrong");
+		}
+	}
+
+	public static Test suite() {
+
+		return new TestSuite(ReadAndSortTest.class);
+	}
+
+	public void testApp() {
+
+		TestRunner runner = new TestRunner();
+		TestSuite suite = new TestSuite();
+
+		suite.addTest(new ReadAndSortTest("testWriteAndSort"));
+
+		suite.addTest(new ReadAndSortTest("testWriteAndSortAgain"));
+
+		runner.doRun(suite);
+	}
+
 }
